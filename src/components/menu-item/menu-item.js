e71d36676d14dc249ebf0eae13df0e78dc5c3b4a
Akshay Patil <patilakshay56.com>
2020-11-18 20:04:44 +0530

    added menu items

---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './menu-item.scss';
+
+const menuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}></div>
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>Shop Now </span>
+      </div>
+    </div>
+  );
+};
+
+export default menuItem;
