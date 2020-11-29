138df5dee301156f5ac36c9a4e671a7cda5886c2
Akshay Patil <patilakshay56.com>
2020-11-29 18:59:46 +0530

    added code to show the cart dropdown

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button';
 import './cart-dropdown.scss';
+import CartItem from '../cart-item/cart-item';
+import { connect } from 'react-redux';
 
-const cartDropdown = () => {
+const cartDropdown = ({ cartItems }) => {
+  console.log('cart component dropdown re-rendered !!!!');
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>Go To Checkout</CustomButton>
     </div>
   );
 };
 
-export default cartDropdown;
+const mapDispatchToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapDispatchToProps, null)(cartDropdown);
